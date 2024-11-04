1312acb520ad4d9363e30d868ff9cea4e7928b5b
MartinaDell\Studente <mbruni02@gmail.com>
2024-11-04 13:51:02 +0100

    Successfully implemented connection and retrieve of broker alerts

--- app/src/main/java/com/project/skyalert/MainActivity.java
+++ app/src/main/java/com/project/skyalert/MainActivity.java
@@ -1,24 +1,65 @@
 package com.project.skyalert;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
-import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.graphics.Insets;
-import androidx.core.view.ViewCompat;
-import androidx.core.view.WindowInsetsCompat;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
 
+    private EditText IP_ADDRESS;
+    private TextView FEEDBACK_TEXTBOX;
+    private TextView ALERT_TEXTBOX;
+    private String BROKER_URL;
+    private static final String CLIENT_ID = java.util.UUID.randomUUID().toString();
+    private MqttHandler mqttHandler;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        System.out.println("Il programma sta iniziando");
         super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
+        System.out.println("Programma iniziato");
         setContentView(R.layout.activity_main);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
+        Button button = (Button) findViewById(R.id.button);
+        IP_ADDRESS = findViewById(R.id.ipAddressInput);
+        button.setOnClickListener(this);
+    }
+
+    @Override
+    protected void onDestroy() {
+        mqttHandler.disconnect();
+        super.onDestroy();
+    }
+
+    @Override
+    public void onClick(View view) {
+        setContentView(R.layout.activity_screen2);
+        FEEDBACK_TEXTBOX = findViewById(R.id.feedbackTextView);
+        ALERT_TEXTBOX = findViewById(R.id.alertsTextView);
+        BROKER_URL = "tcp://" + IP_ADDRESS.getText().toString() + ":1883";
+        FEEDBACK_TEXTBOX.setText("Connecting to broker " + BROKER_URL);
+        mqttHandler = new MqttHandler();
+        mqttHandler.connect(BROKER_URL,CLIENT_ID);
+        FEEDBACK_TEXTBOX.setText("Connected");
+        mqttHandler.subscribe("nina");
+        mqttHandler.setMessageListener(new MqttHandler.MessageListener() {
+            @Override
+            public void onMessageReceived(String topic, String message) {
+                // Run on the UI thread to update the EditText
+                runOnUiThread(new Runnable() {
+                    @Override
+                    public void run() {
+                        ALERT_TEXTBOX.setText("Topic: " + topic + "\nMessage: " + message);
+                    }
+                });
+            }
         });
+    }
+
+    @Override
+    public void onPointerCaptureChanged(boolean hasCapture) {
+        super.onPointerCaptureChanged(hasCapture);
     }
 }
--- app/src/main/java/com/project/skyalert/MqttHandler.java
+++ app/src/main/java/com/project/skyalert/MqttHandler.java
@@ -1,4 +1,110 @@
 package com.project.skyalert;
 
+import org.eclipse.paho.mqttv5.client.IMqttToken;
+import org.eclipse.paho.mqttv5.client.MqttClient;
+import org.eclipse.paho.mqttv5.client.MqttConnectionOptions;
+import org.eclipse.paho.mqttv5.client.MqttCallback;
+import org.eclipse.paho.mqttv5.client.MqttDisconnectResponse;
+import org.eclipse.paho.mqttv5.client.persist.MemoryPersistence;
+import org.eclipse.paho.mqttv5.common.MqttMessage;
+import org.eclipse.paho.mqttv5.common.MqttException;
+import org.eclipse.paho.mqttv5.common.MqttSubscription;
+import org.eclipse.paho.mqttv5.common.packet.MqttProperties;
+
 public class MqttHandler {
+
+    private MqttClient client;
+    private MessageListener messageListener;
+
+    public interface MessageListener {
+        void onMessageReceived(String topic, String message);
+    }
+
+    public void setMessageListener(MessageListener listener) {
+        this.messageListener = listener;
+    }
+
+    public void connect(String brokerUrl, String clientId) {
+        try {
+            // Set up the persistence layer
+            MemoryPersistence persistence = new MemoryPersistence();
+
+            // Initialize the MQTT client
+            client = new MqttClient(brokerUrl, clientId, persistence);
+
+            // Set up the connection options
+            MqttConnectionOptions connectOptions = new MqttConnectionOptions();
+            connectOptions.setCleanStart(true);  // Replaces setCleanSession in MQTTv5
+            connectOptions.setSessionExpiryInterval(0L);  // Optional: can be set as needed
+
+            // Set the callback to handle messages
+            client.setCallback(new MqttCallback() {
+                @Override
+                public void disconnected(MqttDisconnectResponse disconnectResponse) {
+
+                }
+
+                @Override
+                public void mqttErrorOccurred(MqttException exception) {
+
+                }
+
+                @Override
+                public void messageArrived(String topic, MqttMessage message) throws Exception {
+                    if (messageListener != null) {
+                        messageListener.onMessageReceived(topic, new String(message.getPayload()));
+                    }
+                }
+
+                @Override
+                public void deliveryComplete(IMqttToken token) {
+
+                }
+
+                @Override
+                public void connectComplete(boolean reconnect, String serverURI) {
+
+                }
+
+                @Override
+                public void authPacketArrived(int reasonCode, MqttProperties properties) {
+
+                }
+            });
+
+            // Connect to the broker
+            client.connect(connectOptions);
+
+        } catch (MqttException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void disconnect() {
+        try {
+            client.disconnect();
+        } catch (MqttException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void publish(String topic, String message) {
+        try {
+            MqttMessage mqttMessage = new MqttMessage(message.getBytes());
+            mqttMessage.setQos(1);
+            mqttMessage.setRetained(false);
+            client.publish(topic, mqttMessage);
+        } catch (MqttException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void subscribe(String topic) {
+        try {
+            MqttSubscription subscription = new MqttSubscription(topic);
+            client.subscribe(new MqttSubscription[]{subscription});
+        } catch (MqttException e) {
+            e.printStackTrace();
+        }
+    }
 }
