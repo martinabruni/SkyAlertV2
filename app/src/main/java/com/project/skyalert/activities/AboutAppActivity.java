2e0d74a1c438c97201af9fb7421efed4dab174fb
martinabruni <mbruni02@gmail.com>
2024-11-29 13:48:07 +0100

    link from settings to about the app page

---
+++ app/src/main/java/com/project/skyalert/activities/AboutAppActivity.java
@@ -0,0 +1,17 @@
+package com.project.skyalert.activities;
+
+import android.os.Bundle;
+import android.os.PersistableBundle;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.project.skyalert.R;
+
+public class AboutAppActivity extends AppCompatActivity {
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_aboutapp);
+    }
+}
