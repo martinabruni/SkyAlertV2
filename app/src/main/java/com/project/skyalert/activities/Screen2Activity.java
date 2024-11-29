e971a93fbafa57a1a8c278be741da5eaf1f93fa7
martinabruni <mbruni02@gmail.com>
2024-11-29 09:49:40 +0100

    folder refactory and implemented clear and disconnect button on screen2

--- app/src/main/java/com/project/skyalert/activities/Screen2Activity.java
+++ app/src/main/java/com/project/skyalert/activities/Screen2Activity.java
@@ -1,13 +1,23 @@
-package com.project.skyalert;
+package com.project.skyalert.activities;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageButton;
 import android.widget.LinearLayout;
 import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-public class Screen2Activity extends AppCompatActivity {
+import com.project.skyalert.MqttHandlerFacade;
+import com.project.skyalert.NotificationHelper;
+import com.project.skyalert.R;
+import com.project.skyalert.UIManager;
+
+public class Screen2Activity extends AppCompatActivity implements View.OnClickListener {
     private LinearLayout alertsScrollView;
+    private ImageButton clearButton;
+    private Button disconnectButton;
     private MqttHandlerFacade mqttHandlerFacade;
 
     @Override
@@ -16,6 +26,12 @@ public class Screen2Activity extends AppCompatActivity {
         setContentView(R.layout.activity_screen2);
 
         alertsScrollView = findViewById(R.id.layout);
+        clearButton = findViewById(R.id.clearButton);
+        disconnectButton = findViewById(R.id.disconnectButton);
+
+        clearButton.setOnClickListener(this);
+        disconnectButton.setOnClickListener(this);
+
         NotificationHelper notificationHelper = new NotificationHelper(this);
         mqttHandlerFacade = MqttHandlerFacade.getInstance(notificationHelper); // Pass the dependency
 
@@ -31,6 +47,16 @@ public class Screen2Activity extends AppCompatActivity {
         super.onDestroy();
         if (mqttHandlerFacade != null) {
             mqttHandlerFacade.disconnect();
+        }
+    }
+
+    @Override
+    public void onClick(View v) {
+        int id = v.getId();
+        if (id == R.id.disconnectButton) {
+            finish();
+        } else if (id == R.id.clearButton) {
+            alertsScrollView.removeAllViewsInLayout();
         }
     }
 }
